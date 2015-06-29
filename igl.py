f75058c11ee0959676e2de369a18e961687c1eea
dulrich15 <davidjulrich@gmail.com>
2015-06-29 05:07:02 +0000

    Initial scripts

---
+++ igl.py
@@ -0,0 +1,27 @@
+from sympy import *
+from sympy.abc import *
+
+# P,V,n,R,T = symbols(P,V,n,R,T)
+
+igl = Eq(P*V, n*R*T)
+
+values = {
+    R : 8.3144621,
+    T : 273,
+    P : 101300,
+    n : 1
+}
+
+answers = solve(igl.subs(values),V)
+answer = answers[0]
+
+print ""
+print "The ideal gas law is ",latex(igl)
+print ""
+print "Where the gas constant R is {} in SI units".format(values[R])
+print "Standard temperature is {} Kelvin, or 0 degrees Celsius".format(values[T])
+print "Standard pressure is {} pascals, or 1 atm".format(values[P])
+print ""
+print "So, at standard temperature and pressure, one mole of an ideal gas"
+print " occupies {} cubic meters of volume".format(answer)
+print ""
---
+++ kin.py
@@ -0,0 +1,45 @@
+from sympy import *
+from sympy.abc import *
+
+x0, v0 = symbols('x0 v0')
+
+kin1 = Eq(x, x0 + v0 * t + Rational(1,2) * a * t**2)
+kin2 = Eq(v, v0 + a * t)
+
+print ""
+print "The basic equations for one-dimensional motion are:"
+print ""
+print "    {}".format(latex(kin1))
+print "    {}".format(latex(kin2))
+print ""
+print "How long does it take an object released from rest to fall five meters?"
+print ""
+
+values = dict()
+values[x0] = 5
+values[v0] = 1
+values[x] = 0
+values[a] = -9.81
+
+print "The parameters for this problem are:"
+print ""
+for param, value in values.iteritems():
+    print "    {} = {}".format(param, value)
+print ""
+
+answers = solve(kin1.subs(values),t)
+if answers[0] > 0:
+    answer = answers[0]
+else:
+    answer = answers[1]
+
+print "Therefore the time involved is {} seconds.".format(answer)
+print ""
+
+values[t] = answer
+
+answers = solve(kin2.subs(values),v)
+answer = answers[0]
+
+print "And its final speed is {} m/s.".format(answer)
+print ""
